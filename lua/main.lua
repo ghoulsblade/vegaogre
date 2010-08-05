075c3a713e1e03485a7f61adf94ea18a5c0551a4
ghoulsblade <ghoulsblade@schattenkind.net>
2010-08-05 20:56:04 +0200

    first successfull empty main.lua run

--- lua/main.lua
+++ lua/main.lua
@@ -1,3 +1,38 @@
 -- VegaStrike port/rewrite using ogre, see README.txt
 -- VegaStrike Main Website : http://vegastrike.sourceforge.net
-print("hello world!")
+
+--###############################
+--###        CONSTANTS        ###
+--###############################
+
+-- Directories/Files
+gMainWorkingDir     = GetMainWorkingDir and GetMainWorkingDir() or ""
+gBinPath			= gMainWorkingDir.."bin/"  -- bin folder with config etc
+datapath            = gMainWorkingDir.."data/"
+libpath             = gMainWorkingDir.."lua/"
+
+lugreluapath        = (file_exists(gMainWorkingDir.."mylugre") and gMainWorkingDir.."mylugre/lua/" or GetLugreLuaPath()) -- this is should also in USERHOME dir
+
+--###############################
+--###     OTHER LUA FILES     ###
+--###############################
+
+-- utils first
+print("MainWorkingDir",gMainWorkingDir)
+print("lugreluapath",lugreluapath)
+dofile(lugreluapath .. "lugre.lua")
+lugre_include_libs(lugreluapath)
+
+--###############################
+--###        FUNCTIONS        ###
+--###############################
+
+--- called from c right before Main() for every commandline argument
+gCommandLineArguments = {}
+gCommandLineSwitches = {}
+gCommandLineSwitchArgs = {} -- gCommandLineSwitchArgs["-myoption"] = first param after -myoption
+function CommandLineArgument (i,s) gCommandLineArguments[i] = s gCommandLineSwitches[s] = i gCommandLineSwitchArgs[gCommandLineArguments[i-1] or ""] = s end
+
+function Main () 
+	print("welcome to vegaogre")
+end
