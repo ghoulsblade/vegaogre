714212936ab6ed99ad6e72bcd10eca4fdd4437a7
ghoulsblade <ghoulsblade@schattenkind.net>
2010-08-14 16:25:46 +0200

    added ogre linux compiled libs .so.1.7.1 and added LD_LIBRARY_PATH to start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+echo "envvar: $LD_LIBRARY_PATH"
 pushd bin
+LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH
+
 #../src/vegaogre
 # start, and transmit all parameters
 # note : http://tldp.org/LDP/abs/html/othertypesv.html  : above 9: ${10} all: $* and $@ 
